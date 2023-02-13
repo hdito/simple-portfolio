f81e7e2299e2bfedf7c4152dd04566df99674135
hdito <hditow@gmail.com>
2023-02-13 21:34:25 +0300

    Create Menu component

---
+++ src/fonts/hubballi.ts
@@ -0,0 +1,3 @@
+import { Hubballi } from "@next/font/google";
+
+export const hubballi = Hubballi({ subsets: ["latin"], weight: "400" });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,17 @@
 import Head from "next/head";
+import Image from "next/image";
+import homeStyles from "@/styles/Home.module.scss";
+import { hubballi } from "@/fonts/hubballi";
+import { cairo } from "@/fonts/cairo";
+import { GetStaticProps } from "next";
+import Link from "next/link";
 
-export default function Home() {
+interface HomeProps {
+  name: string;
+  specialization: string;
+}
+
+export default function Home({ name, specialization }: HomeProps) {
   return (
     <>
       <Head>
@@ -9,6 +20,51 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className={homeStyles.home}>
+        <Image
+          className={homeStyles.home__image}
+          src="/profile-picture.png"
+          width={405}
+          height={412}
+          alt=""
+        />
+        <Image
+          className={homeStyles["home__image--mob"]}
+          src="/profile-picture-mob.png"
+          width={295}
+          height={288}
+          alt=""
+        />
+        <div className={homeStyles["home__text-container"]}>
+          <h1 className={`${cairo.className} ${homeStyles.home__header}`}>
+            {name}
+          </h1>
+          <p
+            className={`${hubballi.className} ${homeStyles.home__specialization}`}
+          >
+            {specialization}
+          </p>
+          <div
+            className={`${hubballi.className} ${homeStyles["home__links-container"]}`}
+          >
+            <Link className={homeStyles.home__link} href="/resume">
+              Resume
+            </Link>
+            <Link className={homeStyles.home__link} href="/contact">
+              Contact
+            </Link>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps<HomeProps> = () => {
+  return {
+    props: {
+      name: "Name Surname",
+      specialization: "Designer",
+    },
+  };
+};
